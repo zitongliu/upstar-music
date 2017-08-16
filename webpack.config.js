c9778c4cb794a8497ec2d5c82c54ad3a84ab6a83
zitongliu <zitongliu7@gmail.com>
2017-08-16 11:23:17 +1000

    implemented code splitting / caching for vendor

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 var webpack = require('webpack');
 var path = require('path');
 
+const VENDOR_LIBS = [
+  'react', 'lodash', 'redux', 'react-redux', 'react-dom', 'faker', 'react-input-range', 'react-router', 'redux-form', 'redux-thunk'
+];
+
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
@@ -19,5 +26,10 @@ module.exports = {
         test: /\.css$/
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    })
+  ]
 };
